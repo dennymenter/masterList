ff932b886b3e63f7ea10ac72a963e2822d16e7a5
dennymenter <denny_menter@yahoo.com>
2022-05-19 13:20:33 -0500

    nav reworked completely for CMS

--- src/js/links.js
+++ src/js/links.js
@@ -1,7 +1,8 @@
 function getCMSLinks () {
 
-	var menuList = document.getElementById("mainmenuset");
+	var menuList = document.getElementById("navbarSupportedContent");
 	let outputHTML = "";
+	let dropDownIDCounter = 1;
 
 	fetch(
 	  'https://api-us-east-1.graphcms.com/v2/cl3aevzvn0kz401yyd0d8cmc9/master',
@@ -38,26 +39,44 @@ function getCMSLinks () {
 		const  menus  = res.data;
 		console.log(menus);
 
-		outputHTML += '<div id="mainmenuset">'
-
+		outputHTML = `<ul class="navbar-nav">`
 
 		// support one menu level underneath header
 		menus.navMenus.forEach(menu => {
-			// main titles
-			if (menu.link) {
-				outputHTML += `<a href="${menu.link}" target="_blank"><div class="link-header">${menu.menuTitle}</div></a>`	
-			} else {
-				outputHTML += `<div class="link-header">${menu.menuTitle}</div>`
-			}
 
-			//submenus
-			menu.navSubmenus.forEach(submenu => {
-				outputHTML += `<p class="submenu"><a href="${submenu.link}" target="_blank">${submenu.submenuTitle}</a></p>`
+			//are there submenus? If not, just draw a button
+
+			if (!menu.navSubmenus.length == 0) {
+				
+				//dropdown and sublinks
+				outputHTML += `
+				<li class="nav-item dropdown">
+					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown${dropDownIDCounter}" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+					${menu.menuTitle}
+					</a>	
+					<div class="dropdown-menu" aria-labelledby="navbarDropdown${dropDownIDCounter}">
+				`
+				//draw submenus
+				menu.navSubmenus.forEach(submenu => {
+					outputHTML += `
+						<a class="dropdown-item" href="${submenu.link}" target="_blank">${submenu.submenuTitle}</a>
+					`
 					})
-			})
+				dropDownIDCounter ++;
 
-			outputHTML += `</div>`
-			menuList.innerHTML = outputHTML;
+				} else { //button only
+					outputHTML += `
+					<li class="nav-item active ">
+						<a class="nav-link border" href="${menu.link}" target="_blank">${menu.menuTitle}</a>
+					</li>
+					`
+				// finish off LI tag
+				outputHTML += "</div></li>"
+				}
+
 		})
+		outputHTML += `</ul>`;
 
-	};
+		menuList.innerHTML = outputHTML;
+	})
+};
