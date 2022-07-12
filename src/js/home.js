b0582cdde27d903a76de8e141a6d69d0acb3bb39
dennymenter <denny_menter@yahoo.com>
2022-07-12 16:16:17 -0500

    modulizing, trying animations

--- src/js/home.js
+++ src/js/home.js
@@ -1,57 +1,18 @@
-function getHomeElements () {
-
-	var mainTitle = document.getElementById("title");
-	var subtitle = document.getElementById("subtitle");
-	document.body.style.backgroundRepeat= "no-repeat";
-	// document.body.style.backgroundSize= "contain";
+import {getCMSLinks} from "./links.js";
+import {getHomeElements} from "./homeLoad.js";
+import {logMe} from "./logger.js";
 
 
+try {
+	getCMSLinks();	
+} catch (error) {
+	console.log("can't get links: " + error);
+	logMe("can't get links: " + error);
+}
 
-
-	fetch(
-	  'https://api-us-east-1.graphcms.com/v2/cl3aevzvn0kz401yyd0d8cmc9/master',
-	  {
-		method: 'POST',
-		headers: {
-		  'Content-Type': 'application/json',
-		  Accept: 'application/json',
-		},
-		body: JSON.stringify({
-		  query: `
-		  query MyQuery {
-			homePages {
-			  id
-			  title
-			  siteDescription {
-				html
-			  }
-			  backgroundImage {
-				url
-			  }
-			}
-		  }
-		  
-		`,
-		}),
-	  }
-	)
-	  .then((res) => {
-		if (!res.ok) return Promise.reject(response);
-		return res.json();
-	  })
-	  .then((res) => {
-
-		const home  = res.data;
-		// console.log(home.homePages[0].title);
-		// console.log(home.homePages[0].subtitle);
-		// console.log(home.homePages[0].backgroundImage.url);
-		// console.log(home.homePages[0].siteDescription);
-
-		mainTitle.innerText = home.homePages[0].title;
-		subtitle.innerHTML = home.homePages[0].siteDescription.html;
-
-
-		document.body.style.backgroundImage = "url('" + home.homePages[0].
-		backgroundImage.url + "')";
-		})
-	}
+try {
+	getHomeElements();	
+} catch (error) {
+	console.log("can't get home load elememts: " + error);
+	logMe ("can't get home load elememts: " + error);
+}
