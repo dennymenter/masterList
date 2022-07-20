ff5a9f6ef6fe17aad1946e5b80560a27a5c4b06d
dennymenter <denny_menter@yahoo.com>
2022-07-20 08:00:18 -0500

    added tire pressure api

---
+++ js/car.js
@@ -0,0 +1,11 @@
+// get tire pressure
+
+function getTireInfo() {
+    console.log("entering tire info");
+
+    const flTire = document.querySelector("#fl");
+
+    flTire.innerText = "test";
+
+
+}
--- src/js/car.js
+++ src/js/car.js
@@ -1,3 +1,5 @@
+const { convertToHsl } = require("daisyui/src/colors/functions");
+
 function getCar () {
     alert("status!");
 
@@ -6,5 +8,39 @@ function getCar () {
     fetch(url)
     .then(response => response.json())
     .then(data => console.log(data));
+
+}
+
+// get tire pressure
+
+function getTireInfo() {
+    console.log("entering tire info");
+    const flTire = document.getElementById("fl");
+    const frTire = document.getElementById("fr");
+    const rlTire = document.getElementById("rl");
+    const rrTire = document.getElementById("rr");
+
+    const options = {
+        method: 'GET',
+        headers: {
+          Accept: 'application/json',
+          Authorization: 'Bearer H6rLmlHtpGSF3tjfCfpVp9HqKbEf7NDQ'
+        }
+      };
+      
+      fetch('https://api.tessie.com/5YJ3E1EB2NF128926/tire_pressure', options)
+        .then(response => response.json())
+        .then(response => {
+          frontLeft = 
+          frTire.innerText = `Front left pressure = ${response.front_left * 14.503773773}`;
+          flTire.innerText = `Front right pressure = ${response.front_right * 14.503773773}`;
+          rlTire.innerText = `rear left pressure = ${response.rear_left * 14.503773773}`;
+          rrTire.innerText = `rear right pressure = ${response.rear_right * 14.503773773}`;
+        })
+        .catch(err => console.error(err));
+
+       
+        
+
 
 }
